f1bcf43903ecc73788e0f63818bce1aef3ee467a
Adam Ayd <adamayd@gmail.com>
2017-05-13 18:27:47 -0400

    Added hostname section

--- install1.sh
+++ install1.sh
@@ -9,13 +9,16 @@ echo -e "Number of user login : \c" ; who | wc -l
 echo "Calendar"
 cal
 
+# Hostname & Repositories
 echo "Enter your host name"
 read hostname
 echo "You entered: $hostname"
-
+# echo $hostname > /etc/hostname
+# echo -e "127.0.0.1 \t $hostname.localdomain \t $hostname" >> /etc/hosts
 if false
 then
 
+# Users Section
 passwd
 echo "Enter a username:"
 read username
@@ -23,6 +26,7 @@ useradd -m -g users -G wheel,storage,power -s /bin/bash $username
 passwd $username
 visudo
 
+# EFI & Micro Code Section 
 mount -t efivarfs efivarfs /sys/firmware/efi/efivarfs
 bootctl install
 pacman -S intel-ucode
