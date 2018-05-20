750c06031fcf7bd4797fd78805495eaf75ea2e20
Adam Ayd <adamayd@gmail.com>
2018-05-20 13:40:59 -0400

    added SSH keys and agent autoload

--- software.sh
+++ software.sh
@@ -10,20 +10,23 @@ nmcli dev wifi connect $SSID password $WPAPASS
 # Install base CLI software
 sudo pacman -S --noconfirm git lm_sensors tlp htop archey3 openssh exfat-utils vim bash-completion zsh ranger termite yaourt
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Clone dotfiles repo and source files
+cd && git clone https://github.com/adamayd/T460dotfiles.git
 printf "%s\n" "source $HOME/T460dotfiles/zshrc" > $HOME/.zshrc
 printf "%s\n" "source $HOME/T460dotfiles/bashrc" > $HOME/.bashrcz
 printf "%s\n" "so $HOME/T460dotfiles/vimrc" > $HOME/.vimrc
 mkdir $HOME/.config/termite
 ln -s $HOME/T460dotfiles/config/termite/config $HOME/.config/termite/config
+printf "%s\n\t%s\n" "[include]" "path = $HOME/T460dotfiles/gitconfig" > $HOME/.gitconfig 
 
-# Clone repo and start installations by segment
-cd  && git clone https://github.com/adamayd/T460dotfiles.git
-printf "%s\n" "[include]" > $HOME/.gitconfig
-printf "\t%s\n" "path = $HOME/T460dotfiles/gitconfig"
+# Install SSH
+sudo pacman -S --noconfirm openssh
+chmod 755 $HOME/T460dotfiles/xprofile.sh
+printf "%s\n" "$HOME/T460dotfiles/xprofile.sh" > $HOME/.xprofile
 
 # Install XOrg and i3wm
 sudo pacman -S xorg-server xorg-apps i3 feh scrot xclip rofi
-cp ~/T460dotfiles/xprofile ~/.xprofile
 
 # Install Light DM
 sudo pacman -S lightdm lightdm-gtk-greeter
--- xprofile.sh
+++ xprofile.sh
@@ -1,1 +1,5 @@
+#! /bin/bash
+
+eval "$(ssh-agent)"
 cp -f ~/T460dotfiles/config/i3/config ~/.config/i3/config
+
