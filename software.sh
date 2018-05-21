1a7c5a1c9f147e0f424dac0c144196f1285f3f53
Adam Ayd <adamayd@gmail.com>
2018-05-20 23:50:20 -0400

    printer support and updated outline

--- software.sh
+++ software.sh
@@ -7,11 +7,12 @@ read -p 'Enter the SSID of the network: ' SSID
 read -p 'Enter the password: ' WPAPASS
 nmcli dev wifi connect $SSID password $WPAPASS
 
-# Install base CLI software
-sudo pacman -S --noconfirm git lm_sensors tlp htop archey3 openssh exfat-utils vim bash-completion zsh ranger termite yaourt
+# Install CLI Installation Utilities
+pacman -S --noconfirm vim bash-completion zsh ranger termite yaourt lm_sensors tlp htop archey3 exfat-utils 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-# Clone dotfiles repo and source files
+# Clone Dotfiles Repo and Source Files
+pacman -S --noconfirm git
 cd && git clone https://github.com/adamayd/T460dotfiles.git
 printf "%s\n" "source $HOME/T460dotfiles/zshrc" > $HOME/.zshrc
 printf "%s\n" "source $HOME/T460dotfiles/bashrc" > $HOME/.bashrcz
@@ -21,40 +22,62 @@ ln -s $HOME/T460dotfiles/config/termite/config $HOME/.config/termite/config
 printf "%s\n\t%s\n" "[include]" "path = $HOME/T460dotfiles/gitconfig" > $HOME/.gitconfig 
 
 # Install SSH
-sudo pacman -S --noconfirm openssh
+pacman -S --noconfirm openssh
+read -p 'Enter your email address for your SSH Key: ' EMAIL
+ssh-keygen -t rsa -b 4096 -C $EMAIL
 chmod 755 $HOME/T460dotfiles/xprofile.sh
 printf "%s\n" "$HOME/T460dotfiles/xprofile.sh" > $HOME/.xprofile
 
-# Install XOrg and i3wm
-sudo pacman -S xorg-server xorg-apps i3 feh scrot xclip rofi
+# Install XOrg and i3WM
+pacman -S xorg-server xorg-apps i3 feh scrot xclip rofi
 
 # Install Light DM
-sudo pacman -S lightdm lightdm-gtk-greeter
-sudo systemctl enable lightdm.service
+pacman -S --noconfirm lightdm lightdm-gtk-greeter
+systemctl enable lightdm.service
 link /etc/lightdm/ligthdm-gtk-greeter.conf
 *finish avatar and background install*
 
 # Install Audio
-sudo pacman -S alsa-utils pulseaudio
+pacman -S --noconfirm alsa-utils pulseaudio
 amixer sset Master unmute 50
 speaker-test -c 2 -l 2
 
 # Install Intel Video
-sudo pacman -S --noconfirm xf86-video-intel
-sudo ln -s ~/T460dotfiles/remoteconf/20-intel.conf /etc/X11/xorg.conf.d
+pacman -S --noconfirm xf86-video-intel
+ln -s ~/T460dotfiles/remoteconf/20-intel.conf /etc/X11/xorg.conf.d
 
 # Install Synaptics Touchpad
-sudo pacman -S --noconfirm synaptics
-sudo ln -s ~/T460dotfiles/remoteconf/70-synaptics.conf /etc/X11/xorg.conf.d
+pacman -S --noconfirm synaptics
+ln -s ~/T460dotfiles/remoteconf/70-synaptics.conf /etc/X11/xorg.conf.d
 
 # Install System Fonts
-sudo pacman -S ttf-dejavu xorg-fonts-100dpi powerline powerline-fonts
+pacman -S --noconfirm ttf-dejavu xorg-fonts-100dpi powerline powerline-fonts
 yaourt -S system-san-francisco-font-git ttf-font-awesome ttf-ms-fonts ttf-mac-fonts ttf-font-icons
 
-# Install Dev Environment
+# Install Printing Services
+pacman -S --noconfirm cups cups-pdf
+systemctl enable org.cups.cupsd.service
+cp -f $HOME/T460dotfiles/remoteconf/cups-pdf.conf /etc/cups/cups-pdf.conf
+
+# Install CLI Base Software
+pacman -S --noconfirm mpv calendar mutt cmusic
+
+# Install GUI Base Software
+pacman -S --noconfirm pdfviewer printtool scantool tools-needed-for-ranger-above bluez dunst/notifications?? slack gitter transmission calendar mutt
+
+# Install CLI Dev Environment
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 
-# Install LAMP
+# Install GUI Dev Software
+pacamn -S --noconfirm chromium postman vscode slack qutebrowser firefox-developer-edition
 
-# Configure SSH
-#./configssh.sh
+# Install LAMP
+install apache
+copy apached config
+enable apache service
+install php
+install mariadb
+copy mariadb config
+link php to mariadb
+enable mysql service
+
