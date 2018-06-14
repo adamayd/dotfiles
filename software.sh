a26a7324368e5f950b6c7c01d1f481974ddfbaab
Adam Ayd <adamayd@gmail.com>
2018-06-13 23:32:21 -0400

    committing before nuke and pave

--- software.sh
+++ software.sh
@@ -8,7 +8,7 @@ read -p 'Enter the password: ' WPAPASS
 nmcli dev wifi connect $SSID password $WPAPASS
 
 # Install CLI Installation Utilities
-pacman -S --noconfirm vim bash-completion zsh ranger termite yaourt lm_sensors util-linux tlp htop archey3 exfat-utils unzip
+pacman -S --noconfirm vim bash-completion zsh ranger termite yaourt lm_sensors util-linux tlp htop archey3 exfat-utils unzip autofs
 systemctl enable fstrim.timer
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
@@ -30,7 +30,7 @@ chmod 755 $HOME/T460dotfiles/xprofile.sh
 printf "%s\n" "$HOME/T460dotfiles/xprofile.sh" > $HOME/.xprofile
 
 # Install XOrg and i3WM
-pacman -S xorg-server xorg-apps i3 feh scrot xclip rofi
+pacman -S xorg-server xorg-apps i3 feh scrot xclip rofi xcalc
 
 # Install Light DM
 pacman -S --noconfirm lightdm lightdm-gtk-greeter
@@ -61,30 +61,31 @@ systemctl enable org.cups.cupsd.service
 cp -f $HOME/T460dotfiles/remoteconf/cups-pdf.conf /etc/cups/cups-pdf.conf
 
 # Install CLI Base Software
-pacman -S --noconfirm mpv calendar mutt cmusic
+#pacman -S --noconfirm mpv calendar mutt cmusic
 
 # Install GUI Base Software
-pacman -S --noconfirm pdfviewer printtool scantool tools-needed-for-ranger-above bluez dunst/notifications?? transmission calendar mutt qutebrowser 
+#pacman -S --noconfirm pdfviewer printtool scantool tools-needed-for-ranger-above bluez dunst/notifications?? transmission calendar mutt qutebrowser libreoffice
 
 # Install CLI Dev Environment
-pacman -S --noconfirm nodejs nvm npm python python3 pypy pip ruby rvm rbenv mongodb
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+#pacman -S --noconfirm nodejs nvm npm python python3 pypy pip ruby rvm rbenv mongodb
+#curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 
 # Install GUI Dev Software
-pacamn -S --noconfirm chromium postman vscode gitter slack firefox-developer-edition
+pacamn -S --noconfirm chromium firefox-developer-edition atom pycharm-community-edition 
+yaourt -S postman-bin visual-studio-code-bin slack-desktop gitter gitkraken robo3t-bin
 
 # Install LAMP
-install apache
-copy apached config
-enable apache service
-install php
-install mariadb
-copy mariadb config
-link php to mariadb
-enable mysql service
+#install apache
+#copy apached config
+#enable apache service
+#install php
+#install mariadb
+#copy mariadb config
+#link php to mariadb
+#enable mysql service
 
 # Install Android Dev Software
-pacman -S --noconfirm android-studio android-tools android-udev
+#pacman -S --noconfirm android-studio android-tools android-udev mtpfs
 pacman -S --noconfirm bc bison base-devel ccache curl flex png++ gccbase-devel git gnupg gperf imagemagick lib32-ncurses lib32-readline lib32-zlib lz4 ncurses sdl openssl wxgtk3 libxml2 lzop pngcrush rsync schedtool squashfs-tools libxslt zip zlib maven
 yaourt -S esound
 
