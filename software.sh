a83b4aaec56137d3bc94aeff97fc0b7410c38839
Adam Ayd <adamayd@gmail.com>
2018-07-01 01:01:20 -0400

    fixes through base install and some dev env

--- software.sh
+++ software.sh
@@ -8,8 +8,11 @@ read -p 'Enter the password: ' WPAPASS
 nmcli dev wifi connect $SSID password $WPAPASS
 
 # Install CLI Installation Utilities
-pacman -S --noconfirm vim bash-completion zsh ranger termite yaourt lm_sensors git tlp htop archey3 exfat-utils unzip autofs
+pacman -S --noconfirm vim bash-completion zsh ranger termite lm_sensors git tlp htop archey3 exfat-utils unzip autofs
 systemctl enable fstrim.timer
+git clone https://aur.archlinux.org/yay.git
+makepkg -si
+rm -rf yay
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Clone Dotfiles Repo and Source Files
@@ -45,34 +48,44 @@ speaker-test -c 2 -l 2
 
 # Install Intel Video
 pacman -S --noconfirm xf86-video-intel
-ln -s ~/T460dotfiles/remoteconf/20-intel.conf /etc/X11/xorg.conf.d
+cp -f ~/T460dotfiles/remoteconf/20-intel.conf /etc/X11/xorg.conf.d
 
 # Install Synaptics Touchpad
 pacman -S --noconfirm synaptics
-ln -s ~/T460dotfiles/remoteconf/70-synaptics.conf /etc/X11/xorg.conf.d
+cp -f ~/T460dotfiles/remoteconf/70-synaptics.conf /etc/X11/xorg.conf.d
 
 # Install System Fonts
 pacman -S --noconfirm ttf-dejavu xorg-fonts-100dpi powerline powerline-fonts
-yaourt -S system-san-francisco-font-git ttf-font-awesome ttf-ms-fonts ttf-mac-fonts ttf-font-icons
+yay -S system-san-francisco-font-git ttf-font-awesome ttf-ms-fonts ttf-mac-fonts ttf-font-icons
 
 # Install Printing Services
 pacman -S --noconfirm cups cups-pdf
 systemctl enable org.cups.cupsd.service
 cp -f $HOME/T460dotfiles/remoteconf/cups-pdf.conf /etc/cups/cups-pdf.conf
+# install Samsung drivers
+# network printing
+
+# Install Scanning Services
+# SANE services
+# Brother drivers brscan4
+# network scanning
+# gscan2pdf 
 
 # Install CLI Base Software
-#pacman -S --noconfirm mpv calendar mutt cmusic
+pacman -S --noconfirm mpv neomutt calcurse cmus w3m transmission-cli perl-image-exiftool
 
 # Install GUI Base Software
-#pacman -S --noconfirm pdfviewer printtool scantool tools-needed-for-ranger-above bluez dunst/notifications?? transmission calendar mutt qutebrowser libreoffice
+pacman -S --noconfirm qutebrowser libreoffice-fresh transmission-gtk pdfsam # tools-needed-for-ranger-above bluez dunst/notifications??
 
 # Install CLI Dev Environment
-#pacman -S --noconfirm nodejs nvm npm python python3 pypy pip ruby rvm rbenv mongodb
-#curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+source ~/T460dotfiles/nvmrc && nvm install --lts
+pacman -S --noconfirm python-pip python2-pip ruby mongodb
+yay -S rbenv
 
 # Install GUI Dev Software
-pacamn -S --noconfirm chromium firefox-developer-edition atom pycharm-community-edition 
-yaourt -S postman-bin visual-studio-code-bin slack-desktop gitter gitkraken robo3t-bin
+pacamn -S --noconfirm chromium firefox-developer-edition pycharm-community-edition 
+yay -S postman-bin visual-studio-code-bin slack-desktop gitter gitkraken robo3t-bin
 
 # Install LAMP
 #install apache
@@ -87,5 +100,5 @@ yaourt -S postman-bin visual-studio-code-bin slack-desktop gitter gitkraken robo
 # Install Android Dev Software
 #pacman -S --noconfirm android-studio android-tools android-udev mtpfs
 pacman -S --noconfirm bc bison base-devel ccache curl flex png++ gccbase-devel git gnupg gperf imagemagick lib32-ncurses lib32-readline lib32-zlib lz4 ncurses sdl openssl wxgtk3 libxml2 lzop pngcrush rsync schedtool squashfs-tools libxslt zip zlib maven
-yaourt -S esound
+yay -S esound
 
