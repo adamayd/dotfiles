d354fac09f6bdd74d0246abc6c8fcf39e37f65d0
Adam Ayd <adamayd@gmail.com>
2018-07-25 23:42:17 -0400

    removed hardcode username and testing breaks

--- getgit.sh
+++ getgit.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
+# Grab github username
+read -p "Enter your github user name: " $GHUSER
+
 # Create repo directory
 mkdir -p ~/source/projects
 cd ~/source/projects
 
 # Get repo names from Github
-curl -k --request GET https://api.github.com/users/adamayd/repos\?per_page\=100 > curlrepos.txt
+curl -k --request GET https://api.github.com/users/$GHUSER/repos\?per_page\=100 > curlrepos.txt
 
 # Clean repo names
 sed -n '/ssh_url/{p;}' curlrepos.txt > curlrepos2.txt
 sed 's/^\ \ \ \ "ssh_url":\ "//' curlrepos2.txt > curlrepos.txt
 sed 's/",//' curlrepos.txt > curlrepos2.txt
-
-read -p "Waiting"
 
 # Git clone each repo
 while IFS='' read -r REPO || [[ -n "$line" ]]; do
