1b711069d015996a72042b03b7878ad7d830108d
Adam Ayd <adamayd@gmail.com>
2018-08-21 23:51:03 -0400

    moved to systemd ran ssh-agent

--- sshsetup.sh
+++ sshsetup.sh
@@ -4,7 +4,12 @@
 read -p "Enter the email address you want associated with your SSH key: " $EMAILSSH 
 
 ssh-keygen -t rsa -b 4096 -C "$EMAILSSH"
+echo "AddKeysToAgent yes" >> $HOME/.ssh/config
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa
+
+mkdir -p $HOME/.config/systemd/user/
+cp $HOME/T460dotfiles/config/systemd/user/ssh-agent.service $HOME/.config/systemd/user/
+systemctl --user enable ssh-agent
 
 exit 0
