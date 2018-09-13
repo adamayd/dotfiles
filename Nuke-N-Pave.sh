25d29b1e390a0dc8beb1f763bf330cb162f30260
Adam Ayd <adam.ayd@gmail.com>
2018-09-13 19:31:35 -0400

    created Nuke-N-Pave script

---
+++ Nuke-N-Pave.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+# Copy Cutebrowser Configs
+mkdir -p ~/T460dotfiles/config/qutebrowser
+cp ~/.config/qutebrowser/autoconfig.yml ~/T460dotfiles/config/qutebrowser
+cp -r ~/.config/qutebrowser/bookmarks ~/T460dotfiles/config/qutebrowser
+
