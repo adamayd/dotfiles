9bd19e9a1b48710545168a787b04cb08c195b4ec
Adam Ayd <adam.ayd@gmail.com>
2019-12-03 00:32:47 -0500

    added devel libraries for python projects

--- solus-software.sh
+++ solus-software.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-sudo eopkg install zsh ranger lm_sensors tlp htop unzip git neofetch cmatrix
+sudo eopkg install zsh ranger lm_sensors htop unzip git neofetch cmatrix
 sudo eopkg install gnupg password-store
 sudo eopkg install qutebrowser
 sudo eopkg install font-awesome-ttf font-awesome-4 powerline-font
@@ -19,13 +19,15 @@ sudo snap install vscode --classic
 #sudo eopkg install ansible
 #sudo eopkg install gradle
 #sudo eopkg install pip pipenv
+#sudo eopkg install libffi-devel zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite3-devel readline-devel tk-devel gdbm-devel db4.8-devel libpcap-devel xz-devel expat-devel postgresql-devel
 #sudo eopkg install boto3
 #pip install --user awscli
-#sudo eopkg install tlp libelf linux-headers-current
+#sudo eopkg install tlp libelf linux-current-headers
 #git clone acpi_call package
 #cd acpi_call && make
 #sudo make install
 #cd .. && rm -rf acpi_call
+#sudo tlp start
 #sudo eopkg install elixir stuff (asdf)
 #sudo eopkg install rust stuff
 
