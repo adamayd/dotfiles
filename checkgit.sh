3d4d9a383b2ba92ff74242a0d45573c20e092f8f
Adam Ayd <adam.ayd@gmail.com>
2020-04-24 14:23:35 -0400

    updated checkgit script to remove current directory error

--- checkgit.sh
+++ checkgit.sh
@@ -2,7 +2,7 @@
 
 homedir=$(pwd)
 
-for dir in $(find . -maxdepth 1 -type d)
+for dir in $(find . -mindepth 1 -maxdepth 1 -type d)
 do
   cd $dir
   if ! $(git status | grep 'nothing to commit, working tree clean' > /dev/null); then
